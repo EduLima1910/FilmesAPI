c05882ba46dcf8430535a476894e85fa13c9904c
Torvado <eduardo.lima.gja@hotmail.com>
2022-07-12 13:35:05 -0300

    Colocando restrições de ações do usúario

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,11 +1,16 @@
-﻿namespace FilmesAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
 {
     public class Filme
     {
-
+        [Required(ErrorMessage = "O campo título é obrigatório.")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório.")]
         public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não pode ter mais de 30 caracteres.")]
         public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deveter de 1 a 600 minutos.")]
         public int Duracao { get; set; }
 
     }
