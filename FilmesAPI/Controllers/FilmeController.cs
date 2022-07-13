731485c4bb6b90374cbb124836f1112871d22072
Torvado <eduardo.lima.gja@hotmail.com>
2022-07-13 12:19:17 -0300

    Definindo o retorno das rotas, com boas praticas

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -18,24 +18,33 @@ namespace FilmesAPI.Controllers
 
         [HttpPost]
 
-        public void Adicionafilme([FromBody]Filme filme)
+        public IActionResult Adicionafilme([FromBody]Filme filme)
         {
             filme.Id = id++;
             filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new {id = filme.Id}, filme);
         }
 
         [HttpGet]
-        public IEnumerable<Filme> RecuperaFilmes()
+        public IActionResult RecuperaFilmes()
         {
 
-            return filmes;
+            return Ok(filmes);
 
         }
 
         [HttpGet("{id}")]
-        public Filme RecuperaFilmesPorId(int id)
+        public IActionResult RecuperaFilmesPorId(int id)
         {
-            return filmes.FirstOrDefault(filme => filme.Id == id);
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme != null)
+            {
+
+                return Ok(filme);
+
+            }
+            return NotFound();
         }
 
     }
