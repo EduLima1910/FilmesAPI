082b81db53637284f4466f2278846e3109985061
EduLima1910 <eduardo.lima.gja@hotmail.com>
2022-07-15 13:04:58 -0300

    Atualizando o Controller, e testando o banco de dados

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,6 +1,8 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,31 +14,34 @@ namespace FilmesAPI.Controllers
     public class FilmeController : ControllerBase
     {
 
-        private static List<Filme> filmes = new List<Filme>();
+        private FilmeContext _context;
 
-        private static int id = 1;
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
 
         public IActionResult Adicionafilme([FromBody]Filme filme)
         {
-            filme.Id = id++;
-            filmes.Add(filme);
+            _context.Filmes.Add(filme);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(RecuperaFilmesPorId), new {id = filme.Id}, filme);
         }
 
         [HttpGet]
-        public IActionResult RecuperaFilmes()
+        public IEnumerable RecuperaFilmes()
         {
 
-            return Ok(filmes);
+            return _context.Filmes;
 
         }
 
         [HttpGet("{id}")]
         public IActionResult RecuperaFilmesPorId(int id)
         {
-            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
             if (filme != null)
             {
