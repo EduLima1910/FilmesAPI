a0e260de52a8dafc94a2474958526f84c4749623
EduLima1910 <eduardo.lima.gja@hotmail.com>
2022-07-19 10:41:34 -0300

    Adicionando os Dto's

---
+++ FilmesAPI/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadFilmeDto
+    {
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo título é obrigatório.")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório.")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O gênero não pode ter mais de 30 caracteres.")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deveter de 1 a 600 minutos.")]
+        public int Duracao { get; set; }
+
+        public DateTime HoraDaConsulta { get; set; }
+
+    }
+}
