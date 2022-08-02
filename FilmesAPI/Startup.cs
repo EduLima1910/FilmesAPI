4e12066859f6ef5813de70fa6dbd270bbb0433ab
EduLima1910 <eduardo.lima.gja@hotmail.com>
2022-08-02 18:32:42 -0300

    Criando um cinema com endereço

--- FilmesAPI/Startup.cs
+++ FilmesAPI/Startup.cs
@@ -41,9 +41,9 @@ namespace FilmesAPI
         {
             if (env.IsDevelopment())
             {
+
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FilmesAPI v1"));
+
             }
 
             app.UseHttpsRedirection();
