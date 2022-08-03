41c50a58eb60ee98978414074f407c51c4e4da00
EduLima1910 <eduardo.lima.gja@hotmail.com>
2022-08-03 17:40:26 -0300

    Estabelecendo o relacionamento entre Gerente e Cinema

--- FilmesAPI/Models/Gerente.cs
+++ FilmesAPI/Models/Gerente.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmesAPI.Models
 {
@@ -9,6 +10,7 @@ namespace FilmesAPI.Models
         [Required]
         public int Id { get; set; }
         public string Nome { get; set; }
+        public virtual List<Cinema> Cinemas { get; set; }
 
     }
 }
